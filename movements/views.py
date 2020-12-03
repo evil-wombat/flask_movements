22c3a80c1fef7146796d14ea8ba0d11d1ede5f6d
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2020-12-04 00:39:22 +0100

    Clase jueves

--- movements/views.py
+++ movements/views.py
@@ -1,6 +1,20 @@
 from movements import app
 from flask import render_template
+import csv
 
 @app.route('/')
-def lista_movimientos ():
-   return render_template ('movement_list.html', miTexto='Ya veremos si hay movimientos')
+def lista_ingresos ():
+   fIngresos = open ('movements/data/database.csv', 'r')
+   csvReader= csv.reader(fIngresos, delimiter=',', quotechar= '"')
+   ingresos = list (csvReader)
+   suma = 0
+   for ingreso in ingresos:
+      suma += float(ingreso[2])
+   
+   print (ingresos)
+
+   return render_template ('movement_list.html', datos=ingresos, total = suma)
+
+@app.route('/creaalta')
+def algo ():
+  return 'Aquí ira algo'
