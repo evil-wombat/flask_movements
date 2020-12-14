b26ad5b5e4deb21e15d5204789ee6700e316fe71
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2020-12-14 21:10:40 +0100

    Clase del viernes. Renderizando codigo

---
+++ movements/forms.py
@@ -0,0 +1,2 @@
+from flask_wtf import FlaskForm 
+from wtf_forms import Form, StringField
--- movements/views.py
+++ movements/views.py
@@ -3,37 +3,63 @@ from flask import render_template, request, url_for, redirect
 import csv
 import sqlite3
 
-@app.route('/')
-def lista_ingresos ():
-   conn = sqlite3.connect ('movements/data/database.db') #Conectamos nuestro programa con SQLite
+
+DBfile = 'movements/data/database.db'
+
+def consulta (query, params=()):
+   conn = sqlite3.connect(DBfile)
    c = conn.cursor()
 
-   c.execute ('SELECT fecha, concepto, cantidad, id FROM movements')
-  
-   '''
-   fIngresos = open ('movements/data/database.csv', 'r')
-   csvReader= csv.reader(fIngresos, delimiter=',', quotechar= '"')
-   ingresos = list (csvReader)
-   '''
-   ingresos = c.fetchall()
+   c.execute (query, params)
+   conn.commit()
 
+   filas = c.fetchall()
+   print (filas)
+
+   conn.close()
+
+   if len(filas) == 0:
+      return filas
+   
+   columnNames = []
+   for columnName in c.description:
+      columnNames.append(columnName[0])
+
+   listaDeDiccionarios = []
+
+   for fila in filas:
+      d= {}
+      for ix, columnName in enumerate(columnNames):
+         d[columnName] = fila [ix]
+      listaDeDiccionarios.append (d)
+
+   return listaDeDiccionarios
+
+
+@app.route('/')
+def lista_ingresos ():
+
+   ingresos = consulta ('SELECT fecha, concepto, cantidad, id FROM movements;')
 
    suma = 0
    for ingreso in ingresos:
-      suma += float(ingreso[2])
+      print (ingreso)
+      suma += float(ingreso['cantidad'])
    
-   conn.close()
-
    return render_template ('movement_list.html', datos=ingresos, total = suma)
 
 @app.route('/creaalta', methods= ['GET', 'POST'])
 def nuevoIngreso ():
    if request.method == 'POST':
 
-      conn = sqlite3.connect ('movements/data/database.db')  
-      c = conn.cursor()
+      cantidad = request.form.get('cantidad')
+      try:
+         cantidad = float(cantidad)
+      except ValueError:
+         msgError = 'Cantidad debe ser numérica'
+         return render_template('alta.html', errores = msgError)
 
-      c.execute ('INSERT INTO movements (cantidad, concepto, fecha) VALUES (?, ?, ?);', 
+      consulta ('INSERT INTO movements (cantidad, concepto, fecha) VALUES (?, ?, ?);', 
                (
                   float(request.form.get('cantidad')),
                   request.form.get('concepto'),
@@ -41,21 +67,47 @@ def nuevoIngreso ():
                )
             )
       
-      conn.commit()
-      conn.close()
       return redirect(url_for('lista_ingresos'))
-      
-      '''
-      fIngresos = open ('movements/data/database.csv', 'a', newline='')
-      csWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
-      csWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
-      '''
    
    return render_template ('alta.html')
 
-@app.route('/modifica/<id>', methods = ['GET', 'POST'])
+@app.route("/modifica/<id>", methods = ['GET', 'POST'])
 def modifica_ingreso(id):
-   '''
-   1. consulta el movimiento por id
-   2. render_template (modifica.html, movimiento = resultado de la consulta anterior)
-   '''
+
+   if request.method == 'GET':
+
+      
+      registro = consulta ('SELECT fecha, concepto, cantidad, id FROM movements WHERE id = ?', (id,)) [0]
+
+      return render_template ('modifica.html', registro = registro)
+
+   if request.method == 'POST':
+
+      consulta ('UPDATE movements SET fecha = ?, concepto = ?, cantidad = ? WHERE id= ?', 
+               (
+                  request.form.get('fecha'),
+                  request.form.get('concepto'),
+                  float(request.form.get('cantidad')),
+                  id
+               )
+      )
+               
+
+      return redirect(url_for('lista_ingresos'))
+
+   
+
+
+'''  
+@app.route('/delete', methods = 'POST')
+def eliminar_registro(id):
+
+   conn = sqlite3.connect (DBfile)  
+   c = conn.cursor()
+
+   c.execute ('DELETE FROM WHERE id= id', 
+                          
+   conn.commit()
+   conn.close()
+   return redirect(url_for('lista_ingresos'))
+'''
